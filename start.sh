a0db7753dcc9ca5700118f966979711637fb339f
Sean Lee <leejoneshane@gmail.com>
2017-05-14 22:14:57 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Generate strong DH parameters, if they don't already exist.
+#if [ ! -f /etc/ssl/dhparams.pem ]; then
+#  if [ -f /cache/dhparams.pem ]; then
+#    cp /cache/dhparams.pem /etc/ssl/dhparams.pem
+#  else
+#    openssl dhparam -out /etc/ssl/dhparams.pem 2048
+#    # Cache to a volume for next time?
+#    if [ -d /cache ]; then
+#      cp /etc/ssl/dhparams.pem /cache/dhparams.pem
+#    fi
+#  fi
+#fi
+
+
+# Initial certificate request, but skip if cached
+if [ ! -f /etc/letsencrypt/live/${DOMAIN}/fullchain.pem ]; then
+  certbot --nginx \
+   --domain ${DOMAIN} \
+   --email "${EMAIL}" --agree-tos
+fi
+
+nginx -g daemon off;
